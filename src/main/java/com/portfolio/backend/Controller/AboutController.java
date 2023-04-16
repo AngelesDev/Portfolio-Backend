fc3f8a552f9a6acb475b74d2d4e072ad71f65f8d
María Angeles Valdez <mariangelesvaldez32@gmail.com>
2023-04-16 18:41:44 -0300

    fix

--- src/main/java/com/portfolio/backend/Controller/AboutController.java
+++ src/main/java/com/portfolio/backend/Controller/AboutController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-backend-fjc1.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://maria-valdez.netlify.app"})
 public class AboutController {
     @Autowired IAboutService iaboutService;
     
