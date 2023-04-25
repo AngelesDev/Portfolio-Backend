466b5db0621be962fb5ae010071010fd86871a51
María Angeles Valdez <mariangelesvaldez32@gmail.com>
2023-04-25 01:12:01 -0300

    cors

--- src/main/java/com/portfolio/backend/Controller/SkillController.java
+++ src/main/java/com/portfolio/backend/Controller/SkillController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://maria-valdez.netlify.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://argp-66abf.firebaseapp.com"})
 public class SkillController {
     @Autowired ISkillService iskillService;
     
